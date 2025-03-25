0598e6061d334f485362eb0b6eccf56554b0c98c
Niels-Patrick <73878385+Niels-Patrick@users.noreply.github.com>
2025-03-25 14:10:52 +0000

    Some testing

---
+++ dummy_project/src/Test.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+    import { ref, onMounted } from 'vue'
+
+    const dummy_text = ref("No text")
+    const input_text = ref("")
+
+    function display_text(dum_text: string) {
+        dummy_text.value = dum_text
+    }
+
+    onMounted(() => {
+        console.log(`The initial text is ${dummy_text.value}.`)
+    })
+</script>
+
+<template>
+    <header>
+        <h2>Dummy Project Vue</h2>
+    </header>
+
+    <body>
+        <div>
+            <form @submit.prevent="display_text(input_text)">
+                <label for="dum_text">Enter text:</label><br><br>
+                <input type="text" id="dum_text" name="dum_text" v-model="input_text"><br><br>
+                <input type="submit" value="Submit"><br><br>
+            </form>
+            {{ dummy_text }}
+        </div>
+    </body>
+</template>
--- dummy_project/src/main.ts
+++ dummy_project/src/main.ts
@@ -2,5 +2,16 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import App from './App.vue'
+import Test from './Test.vue'
 
-createApp(App).mount('#app')
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+
+const vuetify = createVuetify({
+    components,
+    directives,
+})
+
+createApp(Test).use(vuetify).mount('#app')
